21d05b2bdc4f47123908c254fcd7a4fc3f1b58de
Andrew <andrewstaveley14@gmail.com>
2024-04-13 00:35:24 -0400

    Added menu screens and choices.

--- library/cli.py
+++ library/cli.py
@@ -1,11 +1,20 @@
 from models.employee import Employee
 from models.issue import Issue
+from models.ticket import Ticket
 from helpers import (
     sweep_up_shop,
     exit_program,
     error_screen,
     new_ticket,
     user_view_tickets,
+    admin_view_all_tickets,
+    admin_edit_ticket,
+    create_employee,
+    edit_employee,
+    delete_employee,
+    edit_tickets_by_employee_id,
+    edit_tickets_by_ticket_no,
+
 )
 def login():
     while True:
@@ -17,7 +26,7 @@ def login():
             employee = Employee.find_by_employee_id(int(employee_login))
             permissions = None
             if employee:
-                permissions = employee[3]
+                permissions = employee.access_level
         if permissions == "admin":
             admin_home(employee)
         elif permissions == "user":
@@ -40,9 +49,9 @@ def admin_home(employee_info):
         if choice == "0":
             login()
         elif choice == "1":
-            pass
+            admin_ticket_search(employee_info)
         elif choice == "2":
-            pass
+            edit_employee_database()
         elif choice == "3":
             pass
         else:
@@ -50,7 +59,7 @@ def admin_home(employee_info):
 
 def admin_menu(employee_info):
     sweep_up_shop()
-    print(f"Welcome Admin: {employee_info[1]}")
+    print(f"Welcome Admin: {employee_info.name}")
     print("What would you like to do today?")
     print("Please enter a number to continue")
     print("0. Logout")
@@ -74,12 +83,60 @@ def user_home(employee_info):
 
 def user_menu(employee_info):
     sweep_up_shop()
-    print(f"Welcome User: {employee_info[1]}")
+    print(f"Welcome User: {employee_info.name}")
     print("What would you like to do today?")
     print("Please enter a number to continue")
     print("0. Logout")
     print("1. Create a New Ticket")
     print("2. View Open Tickets")
 
+def admin_ticket_search(employee_info):
+        admin_ticket_search_menu()
+        choice = input("> ")
+        if choice == "0":
+            pass
+        elif choice == "1":
+            edit_tickets_by_employee_id()
+        elif choice == "2":
+            edit_tickets_by_ticket_no()
+        elif choice == "3":
+            admin_view_all_tickets()
+        elif choice == "4":
+            new_ticket(employee_info)
+        else:
+            error_screen("Invalid Input")
+
+def admin_ticket_search_menu():
+    sweep_up_shop()
+    print("How would you like to locate a ticket?")
+    print("0. Back")
+    print("1. Search by Employee ID")
+    print("2. Search by Ticket Number")
+    print("3. View All Open Tickets")
+    print("4. Open New Ticket")
+
+def edit_employee_database():
+    sweep_up_shop()
+    edit_employee_database_menu()
+    choice = input("> ")
+    if choice == "0":
+        pass
+    elif choice == "1":
+        create_employee()
+    elif choice == "2":
+        edit_employee()
+    elif choice == "3":
+        delete_employee()
+    else:
+        error_screen("Invalid Selection")
+
+def edit_employee_database_menu():
+    print("What would you like to change today?")
+    print("0. Back")
+    print("1. Add a New Employee")
+    print("2. Edit Current Employee")
+    print("3. Terminate Employee")
+
 if __name__ == "__main__":
     login()
+
